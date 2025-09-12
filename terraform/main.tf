4026d3b867fda1dc464e98c925d813da5bd9aeb2
gajenderyadavv <02.y.gajender@gmail.com>
2025-09-12 21:23:44 +0530

    subnet auto update

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,35 +2,45 @@ provider "aws" {
   region = var.aws_region
 }
 
-# -------------------------------
-# ECS Cluster
-# -------------------------------
+#############################
+# Fetch Subnets Dynamically #
+#############################
+data "aws_subnets" "ecs_subnets" {
+  filter {
+    name   = "vpc-id"
+    values = [var.vpc_id]
+  }
+}
+
+################
+# ECS Cluster  #
+################
 resource "aws_ecs_cluster" "this" {
   name = "jenkins-ecs-cluster"
 }
 
-# -------------------------------
-# Security Group for ECS tasks
-# -------------------------------
+##########################
+# Security Group for ECS #
+##########################
 resource "aws_security_group" "ecs_sg" {
   name_prefix = "ecs-sg-"
   description = "Allow HTTP traffic for ECS app"
   vpc_id      = var.vpc_id
 
   ingress {
-    description = "Allow inbound app traffic"
     from_port   = 9000
     to_port     = 9000
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
+    description = "Allow inbound app traffic"
   }
 
   egress {
-    description = "Allow all outbound"
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
+    description = "Allow all outbound"
   }
 
   tags = {
@@ -38,22 +48,24 @@ resource "aws_security_group" "ecs_sg" {
   }
 }
 
-# -------------------------------
-# ECS Task Execution IAM Role
-# -------------------------------
+##############################
+# ECS Task Execution Role    #
+##############################
 resource "aws_iam_role" "ecs_task_execution_role" {
-  name               = "ecs-task-execution-role"
-  assume_role_policy = data.aws_iam_policy_document.ecs_task_assume_role.json
-}
+  name = "ecs-task-execution-role"
 
-data "aws_iam_policy_document" "ecs_task_assume_role" {
-  statement {
-    actions = ["sts:AssumeRole"]
-    principals {
-      type        = "Service"
-      identifiers = ["ecs-tasks.amazonaws.com"]
-    }
-  }
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Service = "ecs-tasks.amazonaws.com"
+        }
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
 }
 
 resource "aws_iam_role_policy_attachment" "ecs_task_execution_role_attach" {
@@ -61,9 +73,9 @@ resource "aws_iam_role_policy_attachment" "ecs_task_execution_role_attach" {
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
 
-# -------------------------------
-# ECS Task Definition
-# -------------------------------
+######################
+# ECS Task Definition #
+######################
 resource "aws_ecs_task_definition" "app" {
   family                   = "my-app-task"
   network_mode             = "awsvpc"
@@ -72,24 +84,28 @@ resource "aws_ecs_task_definition" "app" {
   memory                   = "512"
   execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn
 
-  container_definitions = jsonencode([{
-    name  = "app"
-    image = var.app_image
-    portMappings = [{
-      containerPort = 9000
-      hostPort      = 9000
-      protocol      = "tcp"
-    }]
-  }])
+  container_definitions = jsonencode([
+    {
+      name  = "app"
+      image = var.app_image
+      portMappings = [
+        {
+          containerPort = 9000
+          hostPort      = 9000
+          protocol      = "tcp"
+        }
+      ]
+    }
+  ])
 
   tags = {
     Name = "ecs-app-task"
   }
 }
 
-# -------------------------------
-# ECS Service
-# -------------------------------
+#################
+# ECS Service   #
+#################
 resource "aws_ecs_service" "app" {
   name            = "my-app-service"
   cluster         = aws_ecs_cluster.this.id
@@ -98,14 +114,25 @@ resource "aws_ecs_service" "app" {
   launch_type     = "FARGATE"
 
   network_configuration {
-    subnets          = var.subnet_ids
+    subnets          = data.aws_subnets.ecs_subnets.ids
     assign_public_ip = true
     security_groups  = [aws_security_group.ecs_sg.id]
   }
 
-  depends_on = [aws_ecs_task_definition.app]
+  depends_on = [aws_ecs_task_definition.app, aws_iam_role_policy_attachment.ecs_task_execution_role_attach]
 
   tags = {
     Name = "ecs-app-service"
   }
+}
+
+#################
+# Outputs       #
+#################
+output "ecs_service_name" {
+  value = aws_ecs_service.app.name
+}
+
+output "ecs_task_definition" {
+  value = aws_ecs_task_definition.app.family
 }
